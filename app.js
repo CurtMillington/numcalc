be87015a0ab38b69d974e84e5e177792ff2213d4
Curt Millington <curtmillington@gmail.com>
2022-05-16 16:41:10 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const num1El = document.getElementById('num1');
+const operatorEl = document.getElementById('operator');
+const num2El = document.getElementById('num2');
+const answerEl = num1El.value + num2El.value;
+
+const buttonEl = document.getElementById('button');
+buttonEl.addEventListener('click', () => {
+    const whatTheUserTyped = num1El.value;
+    const whatElseTheUserTyped = num2El.value;
+   
+})
